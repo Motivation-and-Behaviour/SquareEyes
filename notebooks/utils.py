1bcb447b03fdbb94cd21d47cd7867d8840bed15b
Taren Sanders <taren.sanders@acu.edu.au>
2020-10-29 15:57:38 +1100

    Working but slow implementation of Open Images downloader

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,10 +1,13 @@
 import concurrent.futures
 import decimal
+import glob
 import multiprocessing
 import os
 import shutil
 import zipfile
+from collections import Counter
 
+import pandas as pd
 import requests
 from pycocotools.coco import COCO
 from tqdm import tqdm
@@ -12,12 +15,13 @@ from tqdm import tqdm
 multiprocessing.set_start_method("spawn", True)
 
 
+def _truncate_to_str(n, places=6):
+    return str(round(decimal.Decimal(n), places))
+
+
 def download_coco_subset(folder, labels=None, parallel=True, class_file=None):
     def convert_classes(old_id, class_list=None):
         return class_list[old_id]
-
-    def truncate_to_str(n, places=6):
-        return str(round(decimal.Decimal(n), places))
 
     def dwnld_im_and_lbls(
         image,
@@ -66,10 +70,10 @@ def download_coco_subset(folder, labels=None, parallel=True, class_file=None):
 
                     label_items = [
                         str(class_id),
-                        truncate_to_str(x),
-                        truncate_to_str(y),
-                        truncate_to_str(w),
-                        truncate_to_str(h),
+                        _truncate_to_str(x),
+                        _truncate_to_str(y),
+                        _truncate_to_str(w),
+                        _truncate_to_str(h),
                     ]
 
                     label_file.write(" ".join(label_items))
@@ -120,7 +124,6 @@ def download_coco_subset(folder, labels=None, parallel=True, class_file=None):
         shutil.rmtree(os.path.join(folder, "annotations"))
 
     # Check if the folders exists, or make them
-    print("Making folders")
     if not os.path.exists(os.path.join(folder, "images")):
         os.makedirs(os.path.join(folder, "images"))
     if not os.path.exists(os.path.join(folder, "labels")):
@@ -168,4 +171,176 @@ def download_coco_subset(folder, labels=None, parallel=True, class_file=None):
                 t.set_description(f"Working on image id: {image['id']}")
                 t.refresh
                 dwnld_im_and_lbls(image)
+
+
+def download_openimages_subset(
+    folder, labels_dict=None, parallel=True, class_file=None, core_only=True
+):
+    OID_URL = "https://storage.googleapis.com/openimages/2018_04/"
+    labels_file = "class-descriptions-boxable.csv"
+    csv_files = [
+        "train-annotations-bbox.csv",
+        "validation-annotations-bbox.csv",
+        "test-annotations-bbox.csv",
+    ]
+
+    # Check for CSV files and download if missing
+    for csv_file in csv_files + [labels_file]:
+        if not os.path.exists(os.path.join(folder, csv_file)):
+            if csv_file in csv_files:
+                file_url = OID_URL + csv_file.split("-")[0] + "/" + csv_file
+            else:
+                file_url = OID_URL + csv_file
+
+            r = requests.get(file_url, allow_redirects=True, stream=True)
+            total_size = int(r.headers.get("content-length", 0))
+            progress_bar = tqdm(total=total_size, unit="iB", unit_scale=True)
+            with open(os.path.join(folder, csv_file), "wb") as f:
+                for data in r.iter_content(1024):
+                    progress_bar.update(len(data))
+                    f.write(data)
+            progress_bar.close()
+
+    # Combine the csv files into one pandas df
+    df = pd.concat(
+        (
+            pd.read_csv(os.path.join(folder, csv_file)).assign(
+                dataset=csv_file.split("-")[0]  # dataset image is in
+            )
+            for csv_file in csv_files
+        )
+    )
+    df_labels = pd.read_csv(
+        os.path.join(folder, labels_file), header=None, names=["LabelName", "Label"]
+    )
+
+    # Limit labels to those from our list of interest
+    df_labels = df_labels.loc[df_labels["Label"].isin(labels_dict.keys())]
+
+    # Merge and drop unneeded
+    df = df.merge(df_labels, how="right", on="LabelName")
+    df = df.loc[
+        (df["IsGroupOf"] == 0) & (df["IsDepiction"] == 0) & (df["IsInside"] == 0)
+    ]
+
+    # Create list of images to download based on labels
+    image_list = set(list(df["dataset"] + "/" + df["ImageID"] + ".jpg"))
+
+    # Check if the folders exists, or make them
+    print("Making folders")
+    if not os.path.exists(os.path.join(folder, "images")):
+        os.makedirs(os.path.join(folder, "images"))
+    if not os.path.exists(os.path.join(folder, "labels")):
+        os.makedirs(os.path.join(folder, "labels"))
+
+    # Convert the label numbers to match the class file
+    with open(class_file, "r") as f:
+        classes = f.read().split("\n")
+
+    if core_only:
+        # Keep only those in the class list
+        df = df.loc[
+            df["Label"].apply(lambda x: True if labels_dict[x] in classes else False)
+        ]
+    else:
+        for val in labels_dict.values():
+            if val not in classes:
+                classes.append(val)
+        # Create revised class list
+        with open(os.path.join(folder, "new_class_list.txt", "a")) as new_classes:
+            new_classes.write("\n".join(classes))
+
+    df["LabelNum"] = df["Label"].apply(lambda x: classes.index(labels_dict[x]))
+
+    # Function to download an image
+    download_command = (
+        "aws s3 --no-sign-request --only-show-errors cp s3://open-images-dataset/"
+    )
+
+    def dwnld_im(image, images_folder=os.path.join(folder, "images")):
+        if not os.path.exists(os.path.join(images_folder, image.split("/")[-1])):
+            os.system(download_command + image + ' "' + images_folder + '"')
+
+    # Iterate over the image list and download the images
+    print(f"getting images ({len(image_list)})")
+    if parallel:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            list(tqdm(executor.map(dwnld_im, image_list), total=len(image_list)))
+    else:
+        t = tqdm(image_list)
+        for image in t:
+            t.set_description(f"Working on image: {image}")
+            t.refresh
+            dwnld_im(image)
+
+    def create_label(image_id, label_num, x_min, x_max, y_min, y_max, folder=folder):
+        # Check if the image has been downloaded
+        labels_folder = os.path.join(folder, "images")
+        if os.path.exists(os.path.join(labels_folder, image_id + ".jpg")):
+            with open(
+                os.path.join(labels_folder, image_id + ".txt"), "a"
+            ) as label_file:
+                label_items = [
+                    str(label_num),
+                    _truncate_to_str((x_max + x_min) / 2),
+                    _truncate_to_str((y_max + y_min) / 2),
+                    _truncate_to_str(x_max - x_min),
+                    _truncate_to_str(y_max - y_min),
+                ]
+
+                label_file.write(" ".join(label_items))
+                label_file.write("\n")
+
+    # Create the labels for each of the images
+    df.apply(
+        lambda x: create_label(
+            x["ImageID"], x["LabelNum"], x["XMin"], x["XMax"], x["YMin"], x["YMax"]
+        ),
+        axis=1,
+    )
+
+
+def count_instances(folders, class_file, parallel=True):
+    # Get classes first
+    def read_txt(file):
+        with open(file, "r") as f:
+            try:
+                return Counter(list(map(int, f.read().split()[::5])))
+            except ValueError:  # First value isn't a number
+                return None
+
+    if isinstance(folders, str):
+        folders = [folders]
+
+    files = []
+    for folder in folders:
+        files.extend(glob.glob(os.path.join(folder, "*.txt")))
+
+    if parallel:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = list(tqdm(executor.map(read_txt, files), total=len(files)))
+    else:
+        t = tqdm(files)
+        results = []
+        for file in t:
+            t.set_description(f"Working on file: {file}")
+            t.refresh
+            results.append(read_txt(file))
+
+    with open(class_file, "r") as f:
+        classes = f.read().split("\n")
+
+    labels = pd.DataFrame(data=classes, columns=["label"])
+    labels = labels.reindex(columns=["label", "objects", "images"])
+    labels.fillna(0, inplace=True)
+
+    for codes in tqdm(results):
+        if codes == None:
+            continue
+        else:
+            for key in codes:
+                labels.loc[key, "objects"] += codes[key]
+                labels.loc[key, "images"] += 1
+
+    return labels
 
