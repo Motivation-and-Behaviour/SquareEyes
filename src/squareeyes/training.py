1dcc47b8e10af3d452196edecf1721df71ee15b2
Taren Sanders <taren.sanders@acu.edu.au>
2023-07-12 16:39:08 +1000

    Test ultralytics

---
+++ src/squareeyes/training.py
@@ -0,0 +1,15 @@
+
+def make_training_data(root = "."):
+
+  # Fetch custom data
+  # Fetch premade datasets
+    # COCO
+    # Objects365?
+    # OpenImages?
+    # ImageNet?
+  # Restrict premade to relevant classes
+  pass
+
+def make_training_yaml():
+  pass
+
