82e99b1df01a0bbb130343002c75149c190c6176
Taren Sanders <taren.sanders@acu.edu.au>
2023-07-24 16:35:18 +1000

    WIP: add coco conversion steps

---
+++ src/squareeyes/classes.py
@@ -0,0 +1,74 @@
+def load_main_classes():
+    main_classes = {
+        0: {"label": "Desktop Computer", "model": "Screens Only"},
+        1: {"label": "Computer Monitor", "model": "Screens Only"},
+        2: {"label": "Laptop Computer", "model": "Screens Only"},
+        3: {"label": "Mobile Phone", "model": "Screens Only"},
+        4: {"label": "iPod", "model": "Screens Only"},
+        5: {"label": "Tablet", "model": "Screens Only"},
+        6: {"label": "Television", "model": "Screens Only"},
+        7: {"label": "Computer Mouse", "model": "Screens + Indicators"},
+        8: {"label": "Computer Keyboard", "model": "Screens + Indicators"},
+        9: {"label": "Remote", "model": "Screens + Indicators"},
+    }
+
+    return main_classes
+
+
+def load_coco_classes():
+    coco_classes = {
+        62: 6,  # tv        -> Television
+        63: 2,  # laptop    -> Laptop Computer
+        64: 7,  # mouse     -> Computer Mouse
+        65: 9,  # remote    -> Remote
+        66: 8,  # keyboard  -> Computer Keyboard
+        67: 3,  # cellphone -> Mobile Phone
+    }
+
+    return coco_classes
+
+
+def load_obj365_classes():
+    obj365_classes = {
+        61: 3,  #  CellPhone   -> Mobile Phone
+        73: 2,  #  Laptop      -> Laptop Computer
+        106: 8,  # keyboard    -> Computer Keyboard
+        115: 7,  # Mouse       -> Computer Mouse
+        132: 9,  # Remote      -> Remote
+        159: 0,  # ComputerBox -> Desktop Computer
+    }
+
+    return obj365_classes
+
+
+def load_imagenet_classes():
+    imagenet_classes = {
+        "n03180011": 0,  # desktop computer -> Desktop Computer
+        "n03782006": 1,  # monitor -> Computer Monitor
+        "n03642806": 2,  # laptop, laptop computer -> Laptop Computer
+        "n03832673": 2,  # notebook, notebook computer -> Laptop Computer
+        "n02992529": 3,  # cellular telephone, cellular phone, cellphone, cell, mobile phone -> Mobile Phone
+        "n03584254": 4,  # iPod -> iPod
+        "n03485407": 5,  # hand-held computer, hand-held microcomputer -> Tablet
+        "n04404412": 6,  # television, television system -> Television
+        "n04152593": 6,  # screen, CRT screen -> Television
+        "n03793489": 7,  # mouse, computer mouse -> Computer Mouse
+        "n03085013": 8,  # computer keyboard, keypad -> Computer Keyboard
+    }
+
+    return imagenet_classes
+
+
+def load_openimages_classes():
+    openimages_classes = {
+        "/m/02522": 1,  # Computer monitor -> Computer Monitor
+        "/m/01c648": 2,  # Laptop -> Laptop Computer
+        "/m/050k8": 3,  # Mobile phone -> Mobile Phone
+        "/m/0mcx2": 4,  # Ipod -> iPod
+        "/m/07c52": 6,  # Television -> Television
+        "/m/020lf": 7,  # Computer mouse -> Computer Mouse
+        "/m/01m2v": 8,  # Computer keyboard -> Computer Keyboard
+        "/m/0qjjc": 9,  # Remote control -> Remote
+    }
+
+    return openimages_classes
--- src/squareeyes/datasets.py
+++ src/squareeyes/datasets.py
@@ -1,6 +1,9 @@
+import os
 from pathlib import Path
 
 import ultralytics as ul
+from classes import load_coco_classes, load_main_classes
+from tqdm import tqdm
 
 
 def download_coco(path="src/squareeyes/datasets"):
@@ -16,5 +19,30 @@ def download_coco(path="src/squareeyes/datasets"):
         "http://images.cocodataset.org/zips/test2017.zip",  # 7G, 41k images (optional)
     ]
     ul.download(urls, dir=dir / "coco" / "images", threads=3)
-    # Clean up zip files to free up space
-    # Create some kind of flag that download is done
+
+
+def convert_coco(path="src/squareeyes/datasets/coco"):
+    coco_classes = load_coco_classes()
+    pass
+
+
+def convert_single_coco(filepath, mappings):
+    with open(filepath, "r") as file:
+        lines = file.readlines()
+
+    new_lines = []
+    for line in lines:
+        line_parts = line.split()
+        if line_parts[0] in mappings.keys():
+            # Replace the class number
+            line_parts[0] = mappings[line_parts[0]]
+            new_lines.append(" ".join(line_parts) + "\n")
+
+    # If there are no usable classes, delete the file and return the filepath
+    if not new_lines:
+        os.remove(filepath)
+        return filepath
+    else:
+        with open(filepath, "w") as files:
+            file.writelines(new_lines)
+        return None
