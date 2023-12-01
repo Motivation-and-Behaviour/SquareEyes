c67526ac069a43b6be0e059ea6cc320d38bc4e38
Taren Sanders <taren.sanders@acu.edu.au>
2023-12-01 11:35:13 +1100

    Fix predictor class conf arg

--- src/models/predict.py
+++ src/models/predict.py
@@ -5,7 +5,7 @@ class Predictor:
     def __init__(self, weights="models/SquareEyes.pt"):
         self.model = YOLO(weights)
 
-    def predict(self, images, verbose=False, stream=True):
+    def predict(self, images, verbose=False, stream=True, conf=0.25):
         return self.model(
-            images, verbose=False, stream=True, conf=0.25, agnostic_nms=True
+            images, verbose=False, stream=True, conf=conf, agnostic_nms=True
         )
